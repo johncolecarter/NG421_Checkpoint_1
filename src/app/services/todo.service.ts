7addf5999308adc798c225986dd0020570a0d7d6
johncolecarter <colecarter7777@gmail.com>
2020-04-17 16:53:10 -0500

    messing with table

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -7,7 +7,7 @@ export class TodoService {
   todoId = 0;
   todoList: ITodo[] = [
     // example of how to make an item in todo list
-    { title: 'Install Angular CLI', id: this.todoId, status: 'Todo', createdAt: new Date() }
+    { title: 'Install Angular CLI', id: this.todoId, status: 'Todo', createdAt: new Date(), description: '' }
 
   ];
 
