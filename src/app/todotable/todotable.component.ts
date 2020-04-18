1d8fd3f6de2a8357fdbf9bb8cd311c2cf01cd698
johncolecarter <colecarter7777@gmail.com>
2020-04-18 14:34:57 -0500

    added pagination and filtering

--- src/app/todotable/todotable.component.ts
+++ src/app/todotable/todotable.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { ITodo } from '../interfaces/itodo';
 import { TodoService } from '../services/todo.service';
 import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-todotable',
@@ -16,12 +17,20 @@ export class TodotableComponent implements OnInit {
 
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
   constructor(private todoService: TodoService) { }
 
   ngOnInit() {
     this.dataSource = new MatTableDataSource(this.todoService.getTodos());
 
     this.dataSource.sort = this.sort;
+
+    this.dataSource.paginator = this.paginator;
   }
 
 }
