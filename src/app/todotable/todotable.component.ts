50f1b49bfd0f975351150a476a7e9a84c0394408
johncolecarter <colecarter7777@gmail.com>
2020-04-18 14:18:22 -0500

    sort works

--- src/app/todotable/todotable.component.ts
+++ src/app/todotable/todotable.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { ITodo } from '../interfaces/itodo';
 import { TodoService } from '../services/todo.service';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-todotable',
@@ -13,10 +14,14 @@ export class TodotableComponent implements OnInit {
   displayedColumns: string[] = ['id', 'title', 'status', 'description', 'createdAt'];
   dataSource: MatTableDataSource<ITodo>;
 
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
   constructor(private todoService: TodoService) { }
 
   ngOnInit() {
     this.dataSource = new MatTableDataSource(this.todoService.getTodos());
+
+    this.dataSource.sort = this.sort;
   }
 
 }
